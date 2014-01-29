48f49bed500d7d6314abf59636f18d0e9ed5761a
Austin King <shout@ozten.com>
2014-01-29 14:01:08 -0800

    Initial commit on behalf of Mark Tanner

---
+++ README.md
@@ -0,0 +1,13 @@
+# Vindinium Starter for C#
+
+Code by Mark Tanner
+(put on github by me, ping me for his contact info)
+
+## Run with
+
+    client <key> <[training|arena]> <number-of-games-to-play> [server-url]
+
+## Examples:
+
+    client mySecretKey arena 10
+    client mySecretKey training 10 http://localhost:9000
---
+++ vindinium/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vindinium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create the server stuff, when not in training mode, it doesnt matter
+            //what you use as the number of turns
+            ServerStuff serverStuff = new ServerStuff("my secret key", true, 300, null);
+
+            //create the random bot, replace this with your own bot
+            RandomBot bot = new RandomBot(serverStuff);
+
+            //now kick it all off by running the bot.
+            bot.run();
+
+            Console.Out.WriteLine("done");
+
+            Console.Read();
+        }
+    }
+}
