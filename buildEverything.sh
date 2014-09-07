12b098958b6811c0aace163d1c171a15f596dffa
george <simms.george.gs@googlemail.com>
2014-09-08 00:49:29 +0100

    fix build

--- buildEverything.sh
+++ buildEverything.sh
@@ -3,10 +3,3 @@
 xbuild Vindinium.sln
 mdoc-update Vindinium/bin/Debug/Vindinium.dll --import Vindinium/bin/Debug/Vindinium.xml --out xml
 mdoc-export-html xml/ -o en
-if [ -z "$(git branch | grep gh-pages)" ]; then
-  git branch gh-pages
-fi
-git checkout gh-pages
-git add en
-git commit -m "Push new docs"
-git push origin master
