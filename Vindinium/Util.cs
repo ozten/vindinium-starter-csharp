115df2d7f75d12acc19397314a0e865e7bd8e4af
george <simms.george.gs@googlemail.com>
2014-09-07 18:31:16 +0100

    add file

---
+++ Vindinium/Util.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
+namespace Vindinium
+{
+    public static class Util
+    {
+
+        internal static T? JToken2NullableT<T>(IDictionary<string, JToken> inp, string key) where T : struct
+        {
+            if (inp != null)
+            {
+                if (inp.Keys.Contains(key))
+                {
+                    var outp = inp[key];
+                    if (outp == null)
+                    {
+                        return null;
+                    }
+                    else
+                    {
+                        return outp.Value<T>();
+                    }
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                return null;
+            }
+
+
+        }
+
+        internal static T JToken2T<T>(IDictionary<string, JToken> inp, string key) where T : class
+        {
+            if (inp != null)
+            {
+                if (inp.Keys.Contains(key))
+                {
+                    var outp = inp[key];
+                    if (outp == null)
+                    {
+                        return null;
+                    }
+                    else
+                    {
+                        return outp.Value<T>();
+                    }
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
+
