73b60d05edadf41c9fc6e704341e5491fe272045
george <simms.george.gs@googlemail.com>
2014-09-08 00:17:40 +0100

    fix nuget

--- buildEverything.sh
+++ buildEverything.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-nuget restore Vindinium.sln
+./nuget.exe restore Vindinium.sln
 xbuild Vindinium.sln
 mdoc-update Vindinium/bin/Debug/Vindinium.dll --import Vindinium/bin/Debug/Vindinium.xml --out xml
 mdoc-export-html xml/ -o en
