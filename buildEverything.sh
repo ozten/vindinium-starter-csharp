d0b18efc5a9ef2914a5a5d5581e508afb484f08e
george <simms.george.gs@googlemail.com>
2014-09-08 08:07:15 +0100

    run gendarme as part of build process

--- buildEverything.sh
+++ buildEverything.sh
@@ -3,3 +3,4 @@
 xbuild Vindinium.sln
 mdoc-update Vindinium/bin/Debug/Vindinium.dll --import Vindinium/bin/Debug/Vindinium.xml --out xml
 mdoc-export-html xml/ -o en
+gendarme Vindinium/bin/Debug/Vindinium.dll --xml xml/gendarme.xml --html en/gendarme.html
