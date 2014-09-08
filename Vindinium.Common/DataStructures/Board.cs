56e54a08fcf602e2df5e62844b3c3bbf66a17052
Lewis Moten <lewismoten@gmail.com>
2014-09-08 01:52:06 -0400

    Fix mapping attributes. Write map as formatted text.

--- Vindinium.Common/DataStructures/Board.cs
+++ Vindinium.Common/DataStructures/Board.cs
@@ -1,14 +1,30 @@
 ﻿using System.Runtime.Serialization;
+using System.Text;
 
 namespace Vindinium.Common.DataStructures
 {
 	[DataContract]
 	public class Board
 	{
-		[DataMember(Name = "MapText")]
+		[DataMember(Name = "tiles")]
 		public string MapText { get; set; }
 
 		[DataMember(Name = "size")]
 		public int Size { get; set; }
+
+		public override string ToString()
+		{
+			var sb = new StringBuilder();
+			string border = string.Format("+{0}+", "-".PadLeft(Size*2, '-'));
+			sb.AppendLine(border);
+			for (int i = 0; i < Size; i++)
+			{
+				sb.Append("|");
+				sb.Append(MapText.Substring(i*Size*2, Size*2));
+				sb.AppendLine("|");
+			}
+			sb.Append(border);
+			return sb.ToString();
+		}
 	}
 }
